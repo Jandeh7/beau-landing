fe7f6d8100aba1220f3d283e1d65dc5c86fb8c3c
jandeh7 <andehjane@gmail.com>
2020-07-14 23:26:04 +0100

    commit files

--- form.php
+++ form.php
@@ -8,33 +8,33 @@ if (!empty($email)) {
     $dbName = "bj1tzzyeokb4rebj";
     
     //create connection
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
     $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
 
-    if (mysqli_connect_error()) {
-        die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
-    } else {
-        $SELECT = "SELECT email From sample Where email = ? Limit 1";
-        $INSERT = "INSERT Into sample (email) value (?)";
+    
+    $SELECT = "SELECT email From sample Where email = ? Limit 1";
+    $INSERT = "INSERT Into sample (email) value (?)";
 
-        //Prepare statement
-        $stmt = $conn->prepare($SELECT);
-        $stmt->bind_result($email);
-        $stmt->store_result();
-        $rnum = $stmt->num_rows;
+    //Prepare statement
+    $stmt = $conn->prepare($SELECT);
+    $stmt->bind_param("s",$email);
+    $stmt->execute();
+    $stmt->bind_result($email);
+    $stmt->store_result();
+    $rnum = $stmt->num_rows;
 
-        if ($rnum==0) {
-            $stmt->close();
-
-            $stmt = $conn->prepare($INSERT);
-            $stmt->bind_param("s", $email);
-            $stmt->execute();
-            echo "You'll definitely hear from us!";
-        } else {
-            echo "Oops! Someone already submitted this email";
-        }
+    if ($rnum==0) {
         $stmt->close();
-        $conn->close();
+
+        $stmt = $conn->prepare($INSERT);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        echo "You'll definitely hear from us!";
+    } else {
+        echo "Oops! Someone already submitted this email";
     }
+    $stmt->close();
+    $conn->close();
 } else {
     echo "Please enter your email address...";
     die();
