c84d42ae077b017c3a3738300507c9bcd79dc7cb
jandeh7 <andehjane@gmail.com>
2020-07-14 08:42:43 +0100

    add changes

--- form.php
+++ form.php
@@ -1,9 +1,7 @@
 <?php
-$firstname = $_POST['firstname'];
-$gender = $_POST['gender'];
 $email = $_POST['email'];
 
-if (!empty($firstname) || !empty($gender) || !empty($email)) {
+if (!empty($email)) {
     $host = "localhost";
     $dbUsername = "root";
     $dbPassword = "";
@@ -16,7 +14,7 @@ if (!empty($firstname) || !empty($gender) || !empty($email)) {
         die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
     } else {
         $SELECT = "SELECT email From sample Where email = ? Limit 1";
-        $INSERT = "INSERT Into sample (firstname, gender, email) values(?, ?, ?)";
+        $INSERT = "INSERT Into sample (email) value (?)";
 
         //Prepare statement
         $stmt = $conn->prepare($SELECT);
@@ -30,17 +28,17 @@ if (!empty($firstname) || !empty($gender) || !empty($email)) {
             $stmt->close();
 
             $stmt = $conn->prepare($INSERT);
-            $stmt->bind_param("sss", $firstname, $gender, $email);
+            $stmt->bind_param("s", $email);
             $stmt->execute();
-            echo "New record inserted successfully";
+            echo "You'll definitely hear from us!";
         } else {
-            echo "Someone already registered with this email";
+            echo "Oops! Someone already submitted this email";
         }
         $stmt->close();
         $conn->close();
     }
 } else {
-    echo "All fields are required";
+    echo "Please enter your email address...";
     die();
 }
 ?>
