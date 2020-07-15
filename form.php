26f57b83beab6619fb8c9465e56b17da0314400d
jandeh7 <andehjane@gmail.com>
2020-07-15 07:10:22 +0100

    file update

--- form.php
+++ form.php
@@ -1,4 +1,12 @@
 <?php
+/* in Development connection
+$host = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "sampledb";
+*/
+
+//remote database connection
 $host = "e11wl4mksauxgu1w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 $dbUsername = "wqm7659rcpesfhim";
 $dbPassword = "cdpcz61aox5hhu5u";
@@ -13,6 +21,7 @@ if (isset($_POST['submit'])) {
     $email = mysqli_real_escape_string($conn, $email);
     $query = "INSERT INTO sample (email) VALUE (?)";
     $result = mysqli_query($conn, $query);
+    
     if ($result) {
         echo "You'll definitely hear from us!";
     }else {
