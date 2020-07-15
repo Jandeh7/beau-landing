3dd747bc2a936e1f6bc0627d3dbb55269363efbb
jandeh7 <andehjane@gmail.com>
2020-07-15 07:27:40 +0100

    commit files

--- form.php
+++ form.php
@@ -1,34 +1,52 @@
 <?php
-/* in Development connection
-$host = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "sampledb";
-*/
 
-//remote database connection
-$host = "e11wl4mksauxgu1w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$dbUsername = "wqm7659rcpesfhim";
-$dbPassword = "cdpcz61aox5hhu5u";
-$dbName = "bj1tzzyeokb4rebj";
+$email = $_POST['email'];
 
-//create connection
-$conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
-$message = '';
+if (!empty($email)) {
 
-if (isset($_POST['submit'])) {
-    $email = $_POST['email'];
-    $email = mysqli_real_escape_string($conn, $email);
-    $query = "INSERT INTO sample (email) VALUE (?)";
-    $result = mysqli_query($conn, $query);
-    
-    if ($result) {
+    /*in Development connection
+    $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "sampledb";
+    */
+
+    //remote database connection
+    $host = "e11wl4mksauxgu1w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $dbUsername = "wqm7659rcpesfhim";
+    $dbPassword = "cdpcz61aox5hhu5u";
+    $dbName = "bj1tzzyeokb4rebj";
+
+    //create connection
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+
+
+    $SELECT = "SELECT email From sample Where email = ? Limit 1";
+    $INSERT = "INSERT Into sample (email) value (?)";
+
+    //Prepare statement
+    $stmt = $conn->prepare($SELECT);
+    $stmt->bind_param("s",$email);
+    $stmt->execute();
+    $stmt->bind_result($email);
+    $stmt->store_result();
+    $rnum = $stmt->num_rows;
+
+    if ($rnum==0) {
+        $stmt->close();
+
+        $stmt = $conn->prepare($INSERT);
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
         echo "You'll definitely hear from us!";
-    }else {
-        echo "Please enter your email address...";
-        die();
+    } else {
+        echo "Oops! Someone already submitted this email";
     }
-    
+    $stmt->close();
+    $conn->close();
+} else {
+    echo "Please enter your email address...";
+    die();
 }
-
 ?>
+
